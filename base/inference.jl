2ba3d69a7f17703dc80283ebcbb81dda03dd99ec
Tim Holy <tim.holy@gmail.com>
2015-05-17 22:04:00 -0500

    Delete debugging code

--- base/inference.jl
+++ base/inference.jl
@@ -249,9 +249,6 @@ const getfield_tfunc = function (A, s0, name)
     if s <: Tuple && name === Symbol
         return Bottom, true
     end
-    # if s <: Tuple && s.parameters.length == 1 && isvatuple(s)
-    #     return s.parameters[1].parameters[1], true
-    # end
     if isa(A[2],QuoteNode) && isa(A[2].value,Symbol)
         fld = A[2].value
         A1 = A[1]
